0ab48211f45c7d0c8b816ba1e569a7e138592e97
Alexander Young <alexander@lxndryng.com>
2014-04-03 14:09:38 +0100

    Added verification of casts/no casts.

--- binbogami/views/admin.py
+++ binbogami/views/admin.py
@@ -5,7 +5,14 @@ admin = Blueprint("admin", __name__, template_folder="templates")
 @admin.route("/admin/")
 def show_casts():
     if 'username' in session:
-        return render_template("podcast_admin.html")
+        casts = g.sqlite_db.execute("select * from podcasts_header where owner=(?)",
+                                    str(session['uid']))
+        rows = casts.fetchall()
+        castlist = []
+        list = None
+        if rows != []:
+            list = rows
+        return render_template("podcast_admin.html", list=list, nocast="No casts.")
     else:
         #TODO: implement this in a prettier manner.
         abort(401)
