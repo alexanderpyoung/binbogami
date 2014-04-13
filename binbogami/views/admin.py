8ecbb16be732d051f9770e6e2a81ca03225023ff
Alexander Young <alexander@lxndryng.com>
2014-04-13 13:34:21 +0100

    Episode adimistration and some further file processing and DB processing logic added.

--- binbogami/views/admin.py
+++ binbogami/views/admin.py
@@ -18,6 +18,27 @@ def show_casts():
     else:
         #TODO: implement this in a prettier manner.
         abort(401)
+        
+@admin.route("/admin/<castname>")
+def show_eps(castname):
+    if 'username' in session:
+        podcastid = get_id("id, name", castname, session['uid'])
+        if podcastid != None:
+            eps = g.sqlite_db.execute(
+                "select title, description, castfile from podcasts_casts where podcast=(?)",
+                [podcastid[0]]
+            ).fetchall()
+            list = None
+            if eps != []:
+                list = eps
+            return render_template(
+                "ep_admin.html", podcastid=podcastid, list=list, 
+                noep="No episodes.", castname=castname
+            )
+        else:
+            return "You don't own this podcast."
+    else:
+        abort(401)
 
 @admin.route("/admin/new/cast", methods=['POST', 'GET'])
 def new_cast():
@@ -45,7 +66,7 @@ def new_cast():
         #TODO: implement this in a prettier manner.
         abort(401)
         
-@admin.route("/admin/<castname>/new/ep", methods=['POST', 'GET'])
+@admin.route("/admin/<castname>/new", methods=['POST', 'GET'])
 def new_ep(castname):
     if 'username' in session:
         podcastid = get_id("id, name", castname, session['uid'])
@@ -53,22 +74,32 @@ def new_ep(castname):
             return render_template("ep_new.html", podcastid=podcastid)
         elif request.method == "POST":
             if podcastid != None:
-                #TODO: form processing logic; SQL elements; 
-                #      process if filename already exists.
                 ep = request.files['castfile']
-                if ep and allowed_file(ep.filename):
+                query = g.sqlite_db.execute(
+                    "select title from podcasts_casts where title=(?) and podcast=(?)",
+                    [request.form['epname'], podcastid[0]]
+                )
+                result = query.fetchone()
+                if ep and allowed_file(ep.filename) and result == None:
                     new_filename = request.form['epname'] + "." + \
                                                 ep.filename.rsplit('.', 1)[1]
                     filename = secure_filename(new_filename) 
-                    ep.save(
-                        os.path.join(
-                            current_app.config["UPLOAD_FOLDER"], 
-                            filename
-                        )
+                    filepath = os.path.join(
+                                current_app.config["UPLOAD_FOLDER"], 
+                                filename
+                                )
+                    ep.save(filepath)
+                    g.sqlite_db.execute(
+                        "insert into podcasts_casts (podcast, title, description, castfile) values (?,?,?,?)",
+                        [podcastid[0], request.form['epname'], request.form['description'], filepath]
                     )
+                    g.sqlite_db.commit()
+                
                     return "Success."
-                else:
-                    return "Failure."
+                elif not allowed_file(ep.filename):
+                    return "File type not allowed."
+                elif result != None:
+                    return "Cast already exists."
             else:
                 return "Not your podcast."
     else:
@@ -78,16 +109,21 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ["mp3", "ogg", "opus", "spx"]
 
-@admin.route("/admin/delete/cast/<castname>")
+@admin.route("/admin/delete/<castname>")
 def delete_cast(castname):
     if 'username' in session:
         # 1) Does the podcast exist? 2) Does it belong to the logged in user?
-        getid = g.sqlite_db.execute(
-            "select id from podcasts_header where name=(?) and owner=(?)", 
-            [castname, session['uid']]
-        )
         podcastid = get_id("id", castname, session['uid'])
         if podcastid != None:
+            # Get and delete files
+            query = g.sqlite_db.execute(
+                "select castfile from podcasts_casts where podcast=(?)",
+                [podcastid[0]]
+            )
+            results = query.fetchall()
+            for result in results:
+                os.remove(result[0])
+            #Do the associated database operations
             g.sqlite_db.execute(
                 "delete from podcasts_header where name=(?)",
                 [castname]
@@ -97,7 +133,32 @@ def delete_cast(castname):
                 [str(podcastid)]    
             )
             g.sqlite_db.commit()
-            #TODO: Add logic around deleting files.
+            return redirect(url_for('admin.show_casts'))
+        else:
+            return "Sorry, the podcast you are attempting to delete does not exist."
+    else:
+        #TODO: implement this in a prettier manner
+        abort(401)
+        
+@admin.route("/admin/delete/<castname>/<epname>")
+def delete_ep(castname, epname):
+    if 'username' in session:
+        # 1) Does the episode exist? 2) Does it belong to the logged in user?
+        podcastid = get_id("id", castname, session['uid'])
+        episode = g.sqlite_db.execute(
+            "select castfile from podcasts_casts where podcast=(?) and title=(?)",
+            [podcastid[0], epname]
+        ).fetchall()
+        if podcastid != None and episode != []:
+            # Get and delete file
+            for ep in episode:
+                os.remove(ep[0])
+            #Do the associated database operations
+            g.sqlite_db.execute(
+                "delete from podcasts_casts where podcast=(?) and title=(?)",
+                [podcastid[0], epname]    
+            )
+            g.sqlite_db.commit()
             return redirect(url_for('admin.show_casts'))
         else:
             return "Sorry, the podcast you are attempting to delete does not exist."
